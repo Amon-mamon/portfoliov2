13fdd0e86ec6afdf043fd4883ea6dd4b7842122c
Vince Stephen David <stoicdavid00@gmail.com>
2026-02-10 21:19:06 +0800

    metadata

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,35 @@
 // app/admin/layout.tsx
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Vince | Web Developer | Creating User-Friendly Websites", // Global title
+  description:
+    "Welcome to my portfolio! I'm Vince, a passionate web developer specializing in creating responsive, accessible, and interactive web applications.",
+  openGraph: {
+    type:"website",
+    title: "Vince | Web Developer | Creating User-Friendly Websites",
+    description:
+      "Welcome to my portfolio! I'm Vince, a passionate web developer specializing in creating responsive, accessible, and interactive web applications.",
+    images: [
+      {
+        url: "https://v-devs.vercel.app", // Example Open Graph image
+        width: 1200,
+        height: 630,
+        alt: "Vince's Portfolio",
+      },
+    ],
+    url: "https://v-devs.vercel.app",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: "https://v-devs.vercel.app",
+  },
+};
+
 export default function AdminLayout({
   children,
 }: {
